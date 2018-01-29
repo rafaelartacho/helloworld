32db70bc893cd71d6376c86e3165578fb99b6f10
Rafael Serrano <rafaelartacho@outlook.com>
2018-01-29 17:32:20 -0500

    adding the whole netbeans project

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author AHC5
+ */
+public class HelloWorld  
+  {
+  public static void main(String[] args) 
+   {
+    String message = "Hello World"; 
+    System.out.println(message);
+    }
+    
+}
